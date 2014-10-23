340a8ac21b3253a0fd21b46464e22dc3e24f7d47
julien leroy <julien.leroy9@gmail.com>
2014-10-23 12:23:53 +0200

    modifie README.MD

--- README.md
+++ README.md
@@ -1,40 +1,89 @@
-
-<=======================>
-<  My LeekWars scripts  >
-<=======================>
+#My LeekWars scripts
 
 
 
-Eleveur de poireaux : superzuzu
 
- ============================
-|1 - Mes strategies          |
-|2 - Mes differents scripts  |
-|3 - La team EPSI Nantes     |
- ============================
+
+1 - Mes strategies
+2 - Mes differents scripts  
+3 - La team EPSI Nantes     
 
 --------------------
-1 - Mes strategies :
+Mes strategies
 --------------------
 
-	a - Le tank : 
+	- Le tank : 
 		Stats : Bourré de vie, de la force, un peux d'armure
 		Weapons : Pistol, Shotgun
-		strategie : il peux enquaisser, il s'avance donc vers l'ennemie pour sortir son shotgun.
+		Strategie : il peux enquaisser, il s'avance donc vers l'ennemie pour sortir son shotgun.
+
+	- Le healer :
+		Stats : 
+		Weapon :
+		Strategie :
 
 --------------------------
-2 - Mes diffents scripts :
+Mes diffents scripts
 --------------------------
 
-	les differents scripts sont contenues dans le dossier IA.
+les differents scripts sont contenues dans le dossier IA.
 
 -------------------------
-3 - La team EPSI Nantes :
+La team EPSI Nantes
 -------------------------
 
+La team grossie de jours en jours.
+![alt tag](http://static.leekwars.com/image/emblem/2648.png?1413960114)
 
 
+Elle est aujourd'hui composé de : 
 
+
+★ ♠ Etheirion ==> Support (Complementaire)
+
+♠ Qexqaneh ==> Healer ! Principalement le tank
+
+♠ WladRSP ==> prépare l'annus des poireaux
+
+♠ tocmah ==> Piroman ! il crame les gens ?
+
+♠ superzuzu ==> Tank ! bourré de PV, fait des dégats et encaisse.
+
+♠ Slip2Guerre
+
+♠ Lethgir
+
+♠ scar
+
+♠ kaylleur
+
+♠ QuinteFush
+
+♠ CyRIIX
+
+♠ Brain3d
+
+♠ Imatoumi
+
+♠ Tartignole
+
+♠ Shlaya
+
+♠ Kaws
+
+♠ MoD
+
+♠ Senpai
+
+♠ Slayer
+
+Légende : 
+* ★ = Capitaine d'equipe (Autoproclamé)
+* ♠ = Fait partie de l'equipe de combat
+
+Eleveur de poireaux : superzuzu
+Participez à la revanche des poireaux et soyez parainné par le meilleur fermier : http://leekwars.com?godfather=superzuzu
+![alt tag](http://static.leekwars.com/image/avatar/19796.png?1413971022)
 
 
 AUTHOR : Julien Leroy
--- Scripts/tank.js
+++ Scripts/tank.js
@@ -1,47 +1,68 @@
+/*
+
+Ce code n'est bien entendu pas encore optimisé et meme pas commenté.
+A suivre.
+
+*/
+
+
 // On récupère l'ennemi le plus proche
 var enemy = getNearestEnemy();
 var myPos = getCell();
 var enemyPos = getCell(enemy);
+var chipUsed = false;
 
 var cellToUseWeapon = getCellToUseWeaponOnCell(WEAPON_SHOTGUN, enemyPos);
 
+if (getTurn() < 2) {
+	say("Blanchard, un margoulin Faisant le fièr a bras !");
+}
+
 var sayTab;
 
-sayTab[1] = "Blanchard, un margoulin près de l'étable !";
+sayTab[1] = "Blanchard, un margoulin Fait le fièr a bras !";
 sayTab[3] = "A L'ASSAUUUUUUUUUUT";
 sayTab[5] = "Bande de chiens galeux !";
 sayTab[10] = "Il faut que j'aille pisser...";
 sayTab[20] = "Bon... on va cleaner tout ça !";
 
-say(sayTab[getTurn()]);
+// say(sayTab[getTurn()]);
 
 // Si l'enemy est loin : on avance vers lui
-if (getDistance(myPos, enemyPos) > 7) {
+if (getDistance(myPos, enemyPos) > 10) {
+	if (getWeapon() != WEAPON_PISTOL) {
+		setWeapon(WEAPON_PISTOL);
+	}
 	moveToward(enemy);
 } else {
 // Si l'enemy est pres : on avance vers lui en ligne.
 // moveTowardLine(myPos, enemyPos);
-	say("je m'en va te fendre de part en part !");
+	//if(canUseChip(CHIP_HELMET, getLeek())){
+		//useChip(CHIP_HELMET, getLeek());
+		//chipUsed = true;
+	//}
+	useChip(CHIP_HELMET, getLeek());
 // pour un plus bas niveau de script, on utilise cette methode :
-	moveToward(cellToUseWeapon);
+
+	moveTowardCell(cellToUseWeapon);
 }
 
-
-// si l'enemie est a + de 7 cube : prendre le pistol
-if (getDistance(myPos, enemyPos) > 7) {
+ if (getDistance(myPos, enemyPos) < 8 && getDistance(myPos, enemyPos) > 5) {
+// si l'enemie est a - de 8 cube : lui casser la geulle avec le flingue puis prendre le fusil a pompe
 	if (getWeapon() != WEAPON_PISTOL) {
 		setWeapon(WEAPON_PISTOL);
 	}
-} else if (getDistance(myPos, enemyPos) < 8) {
-// si l'enemie est a - de 8 cube : lui casser la geulle avec le flingue (3 fois) puis prendre le fusil a pompe
 	useWeapon(enemy);
 	useWeapon(enemy);
 	useWeapon(enemy);
 	if (getWeapon() != WEAPON_SHOTGUN) {
 		setWeapon(WEAPON_SHOTGUN);
 	}
-} else if (getDistance(myPos, enemyPos) < 5){
-// si l'enemie est a - de 5 cube : lui peter les dents avec le shotgun (2fois)
-	useWeapon(WEAPON_SHOTGUN);
-	useWeapon(WEAPON_SHOTGUN);
+} else if (getDistance(myPos, enemyPos) < 6){
+// si l'enemie est a - de 5 cube : lui peter les dents avec le shotgun
+	if (getWeapon() != WEAPON_SHOTGUN) {
+		setWeapon(WEAPON_SHOTGUN);
+	}
+	useWeapon(enemy);
+	useWeapon(enemy);
 }
